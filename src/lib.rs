e9d4d5ba87dfd20df9c5854f78c0e3b3adde5b67
xiaoyu <dianjie1022@gmail.com>
2022-05-22 19:35:02 +0800

    feat: first commit

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use neon::prelude::*;
+use std::io::Cursor;
+
+pub fn player_play(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    let full_url = cx.argument::<JsString>(0)?.value(&mut cx);
+    let resp = reqwest::blocking::get(full_url).unwrap();
+    let cursor = Cursor::new(resp.bytes().unwrap());
+    let source = rodio::Decoder::new(cursor).unwrap();
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+    sink.append(source);
+    sink.play();
+    sink.sleep_until_end();
+    Ok(cx.undefined())
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("playerPlay", player_play)?;
+
+    Ok(())
+}
